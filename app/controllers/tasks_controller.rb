64b50b4f63d83d38ed22349c624ca3366d795b75
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-09-20 23:05:03 +0900

    Edit controller for task add function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,21 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @tasks = Task.new
+#タスクを作るフォームのビューを作成するnew
+  end
+
+  def create
+    @task = Task.create(task_params)
+# 投稿を新規作成。タスクの内容をcreateアクションに送る
+    redirect_to tasks_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
+
 end
